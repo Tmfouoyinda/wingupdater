e218d1f3c172fca110f49f9b503fa1942a399966
t.mfouoyinda <teddynetha1@gmail.com>
2026-04-05 21:10:35 +0200

    Refactor winget updater into a function

--- winget-updater.py
+++ winget-updater.py
@@ -1,34 +1,30 @@
 import os
 
-while True:
-    print("1) Mettre son système à jour")
-    print("2) Mettre ces logiciel à jour")
-    print("3) Quitter")
-    try:
-        choix = int(input("Choix : "))
-    except ValueError:
-        print("Erreur : veuillez entrer un nombre valide.")
-        continue
-    except KeyboardInterrupt:
-        print("\nInterruption détectée. Au revoir !")
-        break
+def winget_updater():
+    while True:
+        print("1) Mettre son système à jour")
+        print("2) Mettre ses logiciels à jour")
+        print("3) Quitter")
 
-
-
-    if choix == 3:
-        print("Au revoir !")
-        break
-    elif choix == 1:
         try:
-            print("Mise à jour du système...")
-            os.system("winget upgrade --id Microsoft.Windows --accept-source-agreements")
+            choix = int(input("Choix : "))
+        except ValueError:
+            print("Erreur : veuillez entrer un nombre valide.")
+            continue
         except KeyboardInterrupt:
-            print("\nMise à jour interrompue.")
-    elif choix == 2:
-        try:
+            print("\nInterruption détectée. Au revoir !")
+            break
+
+        if choix == 1:
+            print("Mise à jour du système...")
+            os.system("start ms-settings:windowsupdate")
+        elif choix == 2:
             print("Mise à jour des logiciels...")
             os.system("winget upgrade --all --accept-source-agreements")
-        except KeyboardInterrupt:
-            print("\nMise à jour interrompue.")
-    else:
-        print("choix invalide, réssayez.")
+        elif choix == 3:
+            print("Au revoir !")
+            break
+        else:
+            print("Choix invalide, réessayez.")
+
+winget_updater()
